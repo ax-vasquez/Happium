2d442793aa02c79b03388840e13579141a57f62f
Armando <ar.xa.vasquez@gmail.com>
2018-01-20 20:36:31 -0600

    Added in Abstraction of the HappiumNode since both hub and nodes are configured from the same general set of settings - added in supplemental classes as well

--- happium-node-service/src/main/java/io/happium/happium_node_service/persistence/HappiumHub.java
+++ happium-node-service/src/main/java/io/happium/happium_node_service/persistence/HappiumHub.java
@@ -3,6 +3,7 @@ package io.happium.happium_node_service.persistence;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
 import org.openqa.grid.internal.utils.configuration.GridHubConfiguration;
 import org.openqa.grid.web.Hub;
 
@@ -27,7 +28,7 @@ import java.io.Serializable;
  */
 @Entity
 @NoArgsConstructor
-public class HappiumHub implements Serializable {
+public class HappiumHub extends HappiumNodeBase {
 
     /**
      * Underlying Hub object that supports this HappiumHub. This is
@@ -69,24 +70,81 @@ public class HappiumHub implements Serializable {
     @Getter @Setter private boolean isRunning;
 
     /**
-     * String representation of the URL where new nodes
-     * can register to this hub
+     * From Selenium Help:
+     *
+     * <p>
+     *     <String> class name : a class implementing the CapabilityMatcher interface.
+     *     Specifies the logic the hub will follow to define whether a request can be
+     *     assigned to a node. For example, if you want to have the matching process
+     *     use regular expressions instead of exact match when specifying browser version.
+     *     ALL nodes of a grid ecosystem would then use the same capabilityMatcher,
+     *     as defined here.
+     * <p>
+     *     Default: org.openqa.grid.internal.utils.DefaultCapabilityMatcher@5fcfe4b2
      */
-    @Column(name = "registration_endpoint", columnDefinition = "text")
-    @Getter @Setter private String registrationEndpointUrl;
+    @Column(name = "capability_matcher", columnDefinition = "text")
+    @Getter @Setter private String capabilityMatcher;
 
     /**
-     * String representation of the endpoint URL to send new WebDriver
-     * requests to for this Hub
+     * From Selenium Help:
+     *
+     * <p>
+     *     <Integer> in ms : The time after which a new test waiting for a node to
+     *     become available will time out. When that happens, the test will throw an
+     *     exception before attempting to start a browser. An unspecified, zero, or
+     *     negative value means wait indefinitely.
+     * <p>
+     *     Default: -1
      */
-    @Column(name = "web_driver_endpoint", columnDefinition = "text")
-    @Getter @Setter private String webDriverEndpointUrl;
+    @Column(name = "new_session_wait_timeout", columnDefinition = "int")
+    @Getter @Setter private int newSessionWaitTimeout;
 
     /**
-     * String representation of the endpoint URL to access this
-     * HappiumHub's backing Hub console
+     * From Selenium Help:
+     *
+     * <p>
+     *     <String> filename: a JSON file (following grid2 format),
+     *     which defines the hub properties
      */
-    @Column(name = "console_endpoint", columnDefinition = "text")
-    @Getter @Setter private String consoleEndpointUrl;
+    @Column(name = "hub_config_file_name", columnDefinition = "text")
+    @Getter @Setter private String hubConfigJsonFileName;
+
+    /**
+     * From Selenium Help:
+     *
+     * <p>
+     *     <String> class name : a class implementing the Prioritizer
+     *     interface. Specify a custom Prioritizer if you want to sort
+     *     the order in which new session requests are processed when
+     *     there is a queue. Default to null ( no priority = FIFO)
+     */
+    @Column(name = "prioritizer", columnDefinition = "text")
+    @Getter @Setter private String prioritizer;
+
+    /**
+     * From Selenium Help:
+     *
+     * <p>
+     *     <String> class name : a class implementing the GridRegistry
+     *     interface. Specifies the registry the hub will use.
+     * <p>
+     *     Default: org.openqa.grid.internal.DefaultGridRegistry
+     */
+    @Column(name = "registry", columnDefinition = "text")
+    @Getter @Setter private String registry;
+
+    /**
+     * From Selenium Help:
+     *
+     * <p>
+     *     <Boolean> true or false : If true, the hub will reject all test requests
+     *     if no compatible proxy is currently registered. If set to false, the
+     *     request will queue until a node supporting the capability is registered
+     *     with the grid.
+     * <p>
+     *     Default: true
+     */
+    @Column(name = "throw_on_capability_not_present", columnDefinition = "boolean")
+    @Getter @Setter private boolean throwOnCapabilityNotPresent;
 
 }
---
+++ happium-node-service/src/main/java/io/happium/happium_node_service/persistence/HappiumNode.java
@@ -0,0 +1,193 @@
+package io.happium.happium_node_service.persistence;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+public class HappiumNode extends HappiumNodeBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "happium_node_id", columnDefinition = "int")
+    @Getter @Setter private Long happiumNodeId;
+
+    /**
+     * From Selenium Help:
+     *
+     * <p>
+     *     <String> : comma separated Capability values.
+     * <p>
+     *     Default:
+     *     [
+     *        Capabilities {
+     *            browserName: chrome,
+     *            maxInstances: 5,
+     *            seleniumProtocol: WebDriver
+     *        },
+     *        Capabilities {
+     *        browserName: firefox,
+     *        marionette: true,
+     *        maxInstances: 5,
+     *        seleniumProtocol: WebDriver
+     *        },
+     *        Capabilities {
+     *        browserName: internet explorer,
+     *        maxInstances: 1,
+     *        platform: WINDOWS,
+     *        seleniumProtocol: WebDriver
+     *        },
+     *        Capabilities {
+     *        browserName: safari,
+     *        maxInstances: 1,
+     *        platform: MAC,
+     *        seleniumProtocol: WebDriver,
+     *        technologyPreview: false
+     *        }
+     *     ]
+     */
+    @Column(name = "capabilities_string", columnDefinition = "text")
+    @Getter @Setter private String capabilitiesString;
+
+    /**
+     * From Selenium Help:
+     *
+     * <p>
+     *     <Integer> : node is marked as "down" if the node
+     *     hasn't responded after the number of checks specified
+     *     in [downPollingLimit].
+     * <p>
+     *     Default: 2
+     */
+    @Column(name = "down_polling_limit", columnDefinition = "int")
+    @Getter @Setter private int downPollingLimit;
+
+    /**
+     * From Selenium Help:
+     *
+     * <p>
+     *     <String> : the url that will be used to post the registration
+     *     request. This option takes precedence over -hubHost and
+     *     -hubPort options.
+     */
+    @Column(name = "hub_url", columnDefinition = "text")
+    @Getter @Setter private String hubUrl;
+
+    /**
+     * From Selenium Help:
+     *
+     * <p>
+     *     <String> IP or hostname : the host address of the hub we're
+     *     attempting to register with. If -hub is specified the -hubHost
+     *     is determined from it.
+     */
+    @Column(name = "hub_host", columnDefinition = "text")
+    @Getter @Setter private String hubHost;
+
+    /**
+     * From Selenium Help:
+     *
+     * <p>
+     *     <Integer> : the port of the hub we're attempting to register
+     *     with. If -hub is specified the -hubPort is determined from it.
+     */
+    @Column(name = "hub_port", columnDefinition = "int")
+    @Getter @Setter private int hubPort;
+
+    /**
+     * From Selenium Help:
+     *
+     * <p>
+     *     <String> : optional unique identifier for the node. Defaults
+     *     to the url of the remoteHost, when not specified.
+     */
+    @Column(name = "node_identifier", columnDefinition = "text")
+    @Getter @Setter private String uniqueNodeIdentifier;
+
+    /**
+     * From Selenium Help:
+     *
+     * <p>
+     *     <Integer> in ms : specifies how often the hub will poll
+     *     to see if the node is still responding.
+     * <p>
+     *     Default: 5000
+     */
+    @Column(name = "node_polling", columnDefinition = "int")
+    @Getter @Setter private int nodePolling;
+
+    /**
+     * From Selenium Help:
+     *
+     * <p>
+     *     <Integer> in ms : connection/socket timeout, used
+     *     for node "nodePolling" check.
+     * <p>
+     *     Default: 5000
+     */
+    @Column(name = "node_status_check_timeout", columnDefinition = "int")
+    @Getter @Setter private int nodeStatusCheckTimeout;
+
+    /**
+     * From Selenium Help:
+     *
+     * <p>
+     *     <String> : the class used to represent the node proxy.
+     * <p>
+     *     Default: org.openqa.grid.selenium.proxy.DefaultRemoteProxy
+     */
+    @Column(name = "proxy", columnDefinition = "text")
+    @Getter @Setter private String proxy;
+
+    /**
+     * From Selenium Help:
+     *
+     * <p>
+     *     if specified, node will attempt to re-register itself
+     *     automatically with its known grid hub if the hub becomes
+     *     unavailable.
+     * <p>
+     *     Default: true
+     */
+    @Column(name = "register", columnDefinition = "boolean")
+    @Getter @Setter private boolean register;
+
+    /**
+     * From Selenium Help:
+     *
+     * <p>
+     *     <Integer> in ms : specifies how often the node will try to
+     *     register itself again. Allows administrator to restart the
+     *     hub without restarting (or risk orphaning) registered nodes.
+     *     Must be specified with the "-register" option.
+     * <p>
+     *     Default: 5000
+     */
+    @Column(name = "register_cycle", columnDefinition = "int")
+    @Getter @Setter private int registerCycle;
+
+    /**
+     * From Selenium Help:
+     *
+     * <p>
+     *     <String> URL: Address to report to the hub. Used
+     *     to override default (http://<host>:<port>).
+     */
+    @Column(name = "remote_host_url", columnDefinition = "text")
+    @Getter @Setter private String remoteHostUrl;
+
+    /**
+     * From Selenium Help:
+     *
+     * <p>
+     *     <Integer> in ms : if the node remains down for more
+     *     than [unregisterIfStillDownAfter] ms, it will stop
+     *     attempting to re-register from the hub.
+     */
+    @Column(name = "unregister_if_still_down_after", columnDefinition = "int")
+    @Getter @Setter private int unregisterIfStillDownAfter;
+
+}
